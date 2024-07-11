3a41474c0af48d2b77a90a8cdfd02fe8a9c407ee
Aleksei Solomatin <Solomatin2006@gmail.com>
2024-07-11 14:04:01 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилия
+    public boolean subscription; // поле для подписки
+}
